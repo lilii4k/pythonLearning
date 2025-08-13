0c249a1b0ec172a2eb45548f8feaea8f3513781e
Lili <lili.mae.martin@gmail.com>
2025-08-13 12:33:39 +0100

    23_Object_oriented_programming

---
+++ 23_Object_oriented_programming.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+car3 = Car("Charger", 2026, "yellow", True)
+
+print(car3.model)
+print(car3.year)
+print(car3.colour)
+print(car3.for_sale)
---
+++ car.py
@@ -0,0 +1,7 @@
+
+class Car:
+    def __init__(self, model, year, colour, for_sale):
+        self.model = model
+        self.year = year
+        self.colour = colour
+        self.for_sale = for_sale
