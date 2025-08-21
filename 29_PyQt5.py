8dac5e39bcb8c069f45c6abff143e563079449bf
lili <lili.mae.martin@gmail.com>
2025-08-21 14:18:36 +0100

    Added image

--- 29_PyQt5.py
+++ 29_PyQt5.py
@@ -1,18 +1,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My first GUI!")
-        self.setGeometry(900, 500, 500, 300)
+        self.setGeometry(900, 500, 800, 600)
         
         label = QLabel("Hello", self)
         label.setFont(QFont("Arial", 30))
         label.setGeometry(10, 10, 70, 30)
-        label.setStyleSheet("color: pink;"
-                            "background-color: grey")
+        label.setStyleSheet("color: orange;"
+                            "background-color: white")
+        label.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+
+        image_label = QLabel(self)
+        pixmap = QPixmap("orangutan.jpg")  # Load your image
+        image_label.setPixmap(pixmap)
+        image_label.setGeometry(30, 30, 500, 500)  # x, y, width, height
+        image_label.setAlignment(Qt.AlignCenter)
+
 
 def main():
     app = QApplication(sys.argv)
